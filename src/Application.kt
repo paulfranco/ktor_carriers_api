e5a44a9be58f949ec98f0bbf2042ac7d54702c55
Paul Franco <mail@paulfran.co>
2021-01-10 03:51:33 -0800

    setup initial features

--- src/Application.kt
+++ src/Application.kt
@@ -1,13 +1,30 @@
 package co.paulfran
 
 import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.gson.*
 import io.ktor.response.*
 import io.ktor.request.*
+import io.ktor.routing.*
+
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
+
+    // Optional Features
+    install(DefaultHeaders)
+    install(CallLogging)
+
+    // Required Features
+    install(Routing)
+    install(ContentNegotiation) {
+        gson {
+            setPrettyPrinting()
+        }
+    }
+
 }
 
