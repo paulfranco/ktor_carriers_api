eec3d6ee2afdf6b122d29d3ed0061cb5f599d5ae
Paul Franco <mail@paulfran.co>
2021-01-10 04:20:08 -0800

    create db and register user

--- src/data/CarriersDatabase.kt
+++ src/data/CarriersDatabase.kt
@@ -1,2 +1,15 @@
 package co.paulfran.data
 
+import co.paulfran.data.collections.Carrier
+import co.paulfran.data.collections.User
+import org.litote.kmongo.coroutine.coroutine
+import org.litote.kmongo.reactivestreams.KMongo
+
+private val client = KMongo.createClient().coroutine
+private val database = client.getDatabase("CarriersDatabase")
+private val users = database.getCollection<User>()
+private val carriers = database.getCollection<Carrier>()
+
+suspend fun registerUser(user: User): Boolean {
+    return users.insertOne(user).wasAcknowledged()
+}
