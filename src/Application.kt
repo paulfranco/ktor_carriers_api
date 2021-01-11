c5bfd4843c4bcc1ce972078b4a7ff2455c54eea0
Paul Franco <mail@paulfran.co>
2021-01-11 01:27:55 -0800

    configure basic authentication

--- src/Application.kt
+++ src/Application.kt
@@ -1,8 +1,10 @@
 package co.paulfran
 
+import co.paulfran.data.checkPasswordForEmail
 import co.paulfran.routes.loginRoute
 import co.paulfran.routes.registerRoute
 import io.ktor.application.*
+import io.ktor.auth.*
 import io.ktor.features.*
 import io.ktor.gson.*
 import io.ktor.response.*
@@ -30,6 +32,22 @@ fun Application.module(testing: Boolean = false) {
             setPrettyPrinting()
         }
     }
+    install(Authentication) {
+        configureAuth()
+    }
 
+}
+
+private fun Authentication.Configuration.configureAuth() {
+    basic {
+        realm = "Carrier Server"
+        validate { credentials ->
+            val email = credentials.name
+            val password = credentials.password
+            if (checkPasswordForEmail(email, password)) {
+                UserIdPrincipal(email)
+            } else null
+        }
+    }
 }
 
